11722e3783550af71996dc2213bb8e5cc1bbf80d
zedricka <zedrick.ara@gmail.com>
2017-10-25 10:00:38 -1000

    Adding Stuff

---
+++ client-side/app.js
@@ -0,0 +1,19 @@
+console.log("Sanity Check");
+
+// API Function
+
+// $("button").click(function(){
+//   $.ajax({
+//     url: "",
+//     type: "GET",
+    
+//     success:function(data){
+
+//     },
+//     fail:function(error){
+//       alert("Error! Data could not be found.");
+//     },
+//   });
+// });
+
+//
