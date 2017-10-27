b71ef6e62394edfa3fec90c59dd9ad87039bd63d
zedricka <zedrick.ara@gmail.com>
2017-10-27 10:38:51 -1000

    I'm getting there

--- handler.js
+++ handler.js
@@ -1,19 +1,22 @@
 'use strict';
-
-module.exports.hello = (event, context, callback) => {
   const AWS = require('aws-sdk');
   const s3 = new AWS.S3();
   const params = { Bucket: 'photo-bucket-tmp-prjct'};
+
+module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'Mark sucks',
       input: event,
     }),
   };
 
   callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+ s3.listObjects(params, function(err, data) {
+   if (err) console.log(err, err.stack); // an error occurred
+   else     console.log(data);           // successful response
+
+
