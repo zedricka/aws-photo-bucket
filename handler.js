3d958240b0904d79139f785f3731a631b6b3a21b
zedricka <zedrick.ara@gmail.com>
2017-10-30 09:33:48 -1000

    Updated 10/30/17

--- handler.js
+++ handler.js
@@ -3,20 +3,19 @@
   const s3 = new AWS.S3();
   const params = { Bucket: 'photo-bucket-tmp-prjct'};
 
+s3.listObjects(params, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
+
 module.exports.hello = (event, context, callback) => {
+console.log("Are you alive");
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Mark sucks',
+      message: 'Testing testing 1 2 3',
       input: event,
     }),
   };
 
   callback(null, response);
 };
-
- s3.listObjects(params, function(err, data) {
-   if (err) console.log(err, err.stack); // an error occurred
-   else     console.log(data);           // successful response
-
-
