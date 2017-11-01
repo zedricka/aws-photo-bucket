7753e27a140110084a3d34064185650aa4994016
zedricka <zedrick.ara@gmail.com>
2017-11-01 10:56:08 -1000

    Everything Works

--- client-side/app.js
+++ client-side/app.js
@@ -2,18 +2,21 @@ console.log("Sanity Check");
 
 // API Function
 
-// $("button").click(function(){
-//   $.ajax({
-//     url: "",
-//     type: "GET",
+let imageNum = [1,2,3,4,5,6,7,8,9,10,11,12];
+let dataType = ['Key'];
+
+$("button").click(function(){
+  $.ajax({
+    url: "https://s3-us-west-2.amazonaws.com/photo-bucket-tmp-prjct/Key",
+    type: "GET",
     
-//     success:function(data){
+    success:function(data){
 
-//     },
-//     fail:function(error){
-//       alert("Error! Data could not be found.");
-//     },
-//   });
-// });
+    },
+    fail:function(error){
+    	alert("Error! Data could not be found.");
+    },
+  });
+});
 
-//
+//https://stackoverflow.com/questions/29488371/how-to-add-a-list-of-images-to-the-document-from-an-array-of-urls
--- handler.js
+++ handler.js
@@ -4,8 +4,9 @@
   const params = { Bucket: 'photo-bucket-tmp-prjct'};
 
 s3.listObjects(params, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(data);           // successful response
+  if (err) console.log(err, err.stack)  // an error occurred
+  else console.log(data);           // successful response
+});
 
 module.exports.hello = (event, context, callback) => {
 console.log("Are you alive");
