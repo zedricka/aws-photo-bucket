69ab587f0e5759086b07048462d8deccfea5403d
zedricka <zedrick.ara@gmail.com>
2017-11-06 10:45:27 -1000

    Finishing Up

--- client-side/app.js
+++ client-side/app.js
@@ -6,16 +6,25 @@ let imageNum = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 $('button').click(function(){
   $.ajax({
-    url: 'https://6yvtnxwo7d.execute-api.us-west-2.amazonaws.com/dev/hello',
+    url: 'https://p4mqwugcd5.execute-api.us-west-2.amazonaws.com/dev/hello',
     type: 'GET',
-    
-    success:function(data){
-    	console.log(data.results);
-    },
-    fail:function(error){
-    	alert("Error! Data could not be found.");
-    },
+    'Content-Type': "application/json"
   })
-});
+	.done((data) =>{
+		console.log(data);
+	})
+    // success: function(data){
+    // 	console.log(data);
+    // 	$(".imageColumn").append("<p></p>")
+    // },
+    .fail((error) =>{
+    	alert("Error! Data could not be found.")
+    	console.log('error', error)
+    })
+    // fail:function(error){
+    // 	alert("Error! Data could not be found.");
+    // },
+  })
+;
 
 //https://stackoverflow.com/questions/29488371/how-to-add-a-list-of-images-to-the-document-from-an-array-of-urls
--- handler.js
+++ handler.js
@@ -3,24 +3,20 @@
   const s3 = new AWS.S3();
   const params = { Bucket: 'photo-bucket-tmp-prjct'};
 
-s3.listObjects(params, function(err, data) {
-  if (err) console.log(err, err.stack)  // an error occurred
-  else console.log(data);           // successful response
-});
 
 module.exports.hello = (event, context, callback) => {
-console.log("Are you alive");
+s3.listObjects(params, function(err, data) {
   const response = {
       statusCode: 200,
       headers: {
-        'Access-Control-Allow-Origin': "*",
+        'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true
       },
       body: JSON.stringify({
-        message: 'Testing testing 1 2 3',
-        input: event,
+        message: data,
       }),
   };
-
   callback(null, response);
-}; 
+}); 
+};
+console.log("Are you alive");
