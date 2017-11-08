f23c6c9ac1c6dc8697b5388ea5deff6bdcc061ca
zedricka <zedrick.ara@gmail.com>
2017-11-08 10:50:03 -1000

    FINISHED WOO HOO CHEE HOO *WHITE VOICE*

--- client-side/app.js
+++ client-side/app.js
@@ -2,8 +2,6 @@ console.log("Sanity Check");
 
 // API Function
 
-let imageNum = [1,2,3,4,5,6,7,8,9,10,11,12];
-
 $('button').click(function(){
   $.ajax({
     url: 'https://p4mqwugcd5.execute-api.us-west-2.amazonaws.com/dev/hello',
@@ -12,19 +10,14 @@ $('button').click(function(){
   })
 	.done((data) =>{
 		console.log(data);
+        data.message.Contents.map((jesus)=>$("#imageColumn").append(`<div class="image"><img src="https://s3-us-west-2.amazonaws.com/photo-bucket-tmp-prjct/${jesus.Key}"</div>`));
+        
 	})
-    // success: function(data){
-    // 	console.log(data);
-    // 	$(".imageColumn").append("<p></p>")
-    // },
-    .fail((error) =>{
-    	alert("Error! Data could not be found.")
+
+        .fail((error) =>{
+    	alert("Error! Data could not be found."),
     	console.log('error', error)
     })
-    // fail:function(error){
-    // 	alert("Error! Data could not be found.");
-    // },
+
   })
 ;
-
-//https://stackoverflow.com/questions/29488371/how-to-add-a-list-of-images-to-the-document-from-an-array-of-urls
